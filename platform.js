d990a8f1316a3df68c04ef8877311c8d6f03a7be
Luca Nisi <luca.nisi@soprasteria.com>
2024-03-07 15:03:25 +0100

    tests

--- platform.js
+++ platform.js
@@ -210,6 +210,7 @@ const BroadlinkRMPlatform = class extends HomebridgePlatform {
       deviceType = isRFSupported ? deviceType | 0x2 : deviceType;
       deviceType = isRM4 ? deviceType | 0x4 : deviceType;
 
+      console.log("LOG____", address, port, mac.toLowerCase(), deviceType);
       broadlink.addDevice({ address, port: 80 }, mac.toLowerCase(), deviceType);
     });
   }
